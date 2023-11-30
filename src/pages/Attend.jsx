6041c43c89ba9997ba1411a8f384bdfd0d30c4dd
Kalid Mohammed <110621255+kalidmd@users.noreply.github.com>
2023-11-30 17:46:05 +0300

    map height modiefied

--- src/pages/Attend.jsx
+++ src/pages/Attend.jsx
@@ -18,7 +18,8 @@ function Attend() {
       </div>
 
       <p className="attend-text">
-        You can attend our introduction class in person for free at our convenient classroom every Sunday from 10:00 AM - 12:00 AM that lasts for an hour and a half. For more information, call us at +2519 11 75 30 88 or +2517 03 93 81 11; You can also <a href="#reg-form">register</a> in the form below to join our introductory class straight away.
+        You can attend our introduction class in person for free at our convenient classroom every Sunday from 10:30 AM - 12:00 AM that lasts for an hour and a half. For more information, call us at +2519 11 75 30 88 or +2517 03 93 81 11. <br /> 
+        You can also <a href="#reg-form">register</a> in the form below to join our introductory class straight away.
       </p>
 
       <Register />
@@ -27,8 +28,6 @@ function Attend() {
           className="map-iframe"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1335.9028812292086!2d38.79457822889989!3d8.991662178880802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85632f825785%3A0x6d746b5b49de4dc1!2z4Yql4YuN4YqQ4Ym14YqVIOGJoOGNjeGJg-GLtSB8IFRydXRoIEJ5IFdpbGw!5e0!3m2!1sen!2set!4v1701344232100!5m2!1sen!2set" 
           title="truth by will location"
-          width="100%" 
-          height="450" 
           allowfullscreen="" 
           style={{border: 0}}
           loading="lazy" 
